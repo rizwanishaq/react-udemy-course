ceb88a3f6c2f0b13cbe63bfacb0605bb7e6cdaa7
rizwanishaq <rizwanishaq@gmail.com>
2019-11-08 18:48:31 +0100

    reach basic-part

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
+
+import { CardList } from "./components/card-list/card-list.component";
+
 import "./App.css";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ""
     };
   }
 
@@ -16,11 +20,18 @@ class App extends Component {
   }
 
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
     return (
       <div className="App">
-        {this.state.monsters.map(monster => (
-          <h1 key={monster.id}>{monster.name}</h1>
-        ))}
+        <input
+          type="search"
+          placeholder="search monsters"
+          onChange={e => this.setState({ searchField: e.target.value })}
+        />
+        <CardList monsters={filteredMonsters}></CardList>
       </div>
     );
   }
