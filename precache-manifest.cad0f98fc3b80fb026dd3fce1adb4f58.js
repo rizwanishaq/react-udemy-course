f8cead491baa889fd661e715d5294bea4ed6dfef
rizwanishaq <rizwanishaq@gmail.com>
2019-11-11 09:58:07 +0100

    Updates

---
+++ precache-manifest.cad0f98fc3b80fb026dd3fce1adb4f58.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f16d50df12f7bcc4b50e178f8ebae01a",
+    "url": "/react-udemy-course/index.html"
+  },
+  {
+    "revision": "59c928d1adb370b6c44b",
+    "url": "/react-udemy-course/static/css/main.4d6eb315.chunk.css"
+  },
+  {
+    "revision": "6df4eb5567d793ed81f5",
+    "url": "/react-udemy-course/static/js/2.975c5e1d.chunk.js"
+  },
+  {
+    "revision": "59c928d1adb370b6c44b",
+    "url": "/react-udemy-course/static/js/main.eef2b81b.chunk.js"
+  },
+  {
+    "revision": "d51a38e4fb4fb3e1d8ad",
+    "url": "/react-udemy-course/static/js/runtime-main.4859d2a8.js"
+  }
+]);
